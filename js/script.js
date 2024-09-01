aa9ee4aef9c1dc8055287ff5235c6324159c59ce
shanom <pitchayakorn.sai@gmail.com>
2024-09-01 22:20:20 +0700

    moredeco

--- js/script.js
+++ js/script.js
@@ -2,3 +2,19 @@ function toggleMenu() {
     var navMenu = document.getElementById('nav-menu');
     navMenu.classList.toggle('active');
 }
+
+
+const images = ['Image/coding.png', 'Image/coding2.png', 'Image/coding3.png', 'Image/coding4.png', 'Image/coding5.png', 'Image/coding6.png'];
+const imgElement = document.getElementById('skillImage');
+let currentIndex = 0;
+
+function changeImage() {
+    imgElement.src = images[currentIndex];
+    currentIndex++;
+    if (currentIndex >= images.length) {
+      currentIndex = 0;
+    }
+  }
+
+window.onload = changeImage;
+setInterval(changeImage, 3000);
